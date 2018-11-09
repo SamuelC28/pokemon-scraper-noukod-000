65a61c563de53fe1d782c746f09b64fd8371028c
Samuel Cornet <samuel@noukod.com>
2018-11-09 15:53:15 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,6 +1,6 @@
 class Pokemon
   @@all = []
-  attr_accessor :name, :type, :db, :id, :hp
+  attr_accessor :name, :type, :database_connection, :id, :hp
 
   def initialize(keyword)
 
@@ -10,4 +10,12 @@ class Pokemon
     database_connection.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
   end
 
+def self.find(num, database_connection)
+#   pokemon = db.execute("SELECT * FROM pokemon WHERE id=?", [num])
+#     new_pokemon = self.new(pokemon)
+#     new_pokemon.id = pokemon[0][0]
+#     new_pokemon.name = pokemon[0][1]
+#     new_pokemon.type = pokemon[0][2]
+#     new_pokemon.hp = pokemon[0][3]
+# return new_pokemon
 end
