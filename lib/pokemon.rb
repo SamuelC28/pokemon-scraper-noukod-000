d0d9411e91e742b92552602e42efc1ec4f7afb11
Samuel Cornet <samuel@noukod.com>
2018-11-09 15:54:55 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -10,7 +10,7 @@ class Pokemon
     database_connection.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
   end
 
-def self.find(num, db)
+# def self.find(num, db)
 #   pokemon = db.execute("SELECT * FROM pokemon WHERE id=?", [num])
 #     new_pokemon = self.new(pokemon)
 #     new_pokemon.id = pokemon[0][0]
@@ -18,5 +18,5 @@ def self.find(num, db)
 #     new_pokemon.type = pokemon[0][2]
 #     new_pokemon.hp = pokemon[0][3]
 # return new_pokemon
-end
+# end
 end
