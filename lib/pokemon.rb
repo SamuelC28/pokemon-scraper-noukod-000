356dd88d6f97d1e37a2d335b22038c95e22bde4d
Samuel Cornet <samuel@noukod.com>
2018-11-09 16:43:16 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -20,16 +20,16 @@ class Pokemon
   return new_pokemon
  end
 
-def self.bonus(pokemon)
-   database_connection = SQLite3::Database.new('db/pets.db')
-
-    # Cat.new("Maru", "scottish fold", 3)
-    # Cat.new("Hana", "tortoiseshell", 1)
-
-    Pokemon.all.each do |pokemon|
-      Pokemon.save(pokemon.name, pokemon.breed, pokemon.age, db)
-    end
-end
-
+# def self.bonus(pokemon)
+#    database_connection = SQLite3::Database.new('db/pets.db')
+# 
+#     # Cat.new("Maru", "scottish fold", 3)
+#     # Cat.new("Hana", "tortoiseshell", 1)
+# 
+#     Pokemon.all.each do |pokemon|
+#       Pokemon.save(pokemon.name, pokemon.breed, pokemon.age, db)
+#     end
+# end
+# 
 
 end
